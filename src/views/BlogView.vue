3aa165120e7545e5324bbc5df34a3a64abb64482
zenenperaza <peraza@outlook.com>
2023-06-05 10:16:37 -0400

    commits 5

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -2,10 +2,10 @@
     <div class="about">
       <h1>This is an Blog page</h1>
       <ul>
-        <li><router-link to=""> Primer post</router-link> </li>
-        <li><router-link to="">2 post</router-link></li>
-        <li><router-link to="">3 post</router-link></li>
-        <li><router-link to="">4 post</router-link></li>
+        <li><router-link :to="{ name: 'post', params: { id: 1} }">Primer post</router-link> </li>
+        <li><router-link :to="{ name: 'post', params: { id: 2} }">2 post</router-link></li>
+        <li><router-link :to="{ name: 'post', params: { id: 4} }">3 post</router-link></li>
+        <li><router-link :to="{ name: 'post', params: { id: 3} }">4 post</router-link></li>
       </ul>
     </div>
   </template>
--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,5 +1,19 @@
 <template>
     <div class="">
       <h1>This is an Post page</h1>
+      <h1>This is the Post number {{ postId }}</h1>
     </div>
   </template>
+
+<script lang="ts" setup>
+import { onMounted, ref  } from "vue";
+import { useRoute } from "vue-router";
+
+let postId = ref(0)
+
+onMounted(() => {
+  const route = useRoute()
+  postId.value = route.params.id
+})
+
+</script>
